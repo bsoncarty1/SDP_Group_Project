43f5ec24e53b3d41011aa84da8706d5d991d4ae7
Brandon Soncarty <bsoncarty@ewu.edu>
2022-11-15 16:26:13 -0800

    added flap method to CommmandParserStructural class

--- src/sbw/project/cli/parser/CommandParserStructural.java
+++ src/sbw/project/cli/parser/CommandParserStructural.java
@@ -3,6 +3,7 @@ package sbw.project.cli.parser;
 import sbw.architecture.datatype.Identifier;
 import sbw.project.cli.action.ActionSet;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CommandParserStructural extends CommandParser{
@@ -22,6 +23,8 @@ public class CommandParserStructural extends CommandParser{
             Elevator(actionSet, textArr);
         }else if(textArr[1].equalsIgnoreCase("AILERON")) {
             Aileron(actionSet, textArr);
+        }else if(textArr[1].equalsIgnoreCase("FLAP")) {
+            Flap(actionSet, textArr);
         }
     }
 
@@ -45,12 +48,25 @@ public class CommandParserStructural extends CommandParser{
         Identifier idn = new Identifier(textArr[6]); //This can have one or more
         Identifier idx = new Identifier(textArr[7]); //This can have 0 or more
         /**TODO: Instructions:
-        //Creates a ControllerAileron with identifier id1 containing n ailerons idn, where n is even. The first half of n in order are on the left wing, and the second half on the right. idx specifies which of idn is the primary one that is directly
-        //commanded by a request to this controller. It must be on the left wing. The others respond based on it: all ailerons on the same side deflect in the same direction, and all on the opposite side deflect in the opposite direction (except when used as a speed brake; see III.3.b). idslave optionally bases its deflection (and likewise its opposite's) on idmaster by percent percent. Any chain of mixing is possible, but there are no cyclical relationships. Aileron configurations must be symmetrically identical.
-        //This calls two variants of doDeclareAileronController(), which creates and registers an instance of ControllerAileron.
-        **/
+         //Creates a ControllerAileron with identifier id1 containing n ailerons idn, where n is even. The first half of n in order are on the left wing, and the second half on the right. idx specifies which of idn is the primary one that is directly
+         //commanded by a request to this controller. It must be on the left wing. The others respond based on it: all ailerons on the same side deflect in the same direction, and all on the opposite side deflect in the opposite direction (except when used as a speed brake; see III.3.b). idslave optionally bases its deflection (and likewise its opposite's) on idmaster by percent percent. Any chain of mixing is possible, but there are no cyclical relationships. Aileron configurations must be symmetrically identical.
+         //This calls two variants of doDeclareAileronController(), which creates and registers an instance of ControllerAileron.
+         **/
 
     }
+
+    public void Flap(ActionSet a, String[] textArr){
+        Identifier id = new Identifier(textArr[3]);
+        int i = 6;
+        ArrayList<Identifier> list = new ArrayList<>();
+        while(i < textArr.length){
+            Identifier idn = new Identifier(textArr[i]);//TODO: this can have one or more
+            list.add(idn);
+            i++;
+        }
+        a.getActionStructural().doDeclareFlapController(id,list);
+    }
+
 
 
 
