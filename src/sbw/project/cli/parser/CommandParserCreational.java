2fbf1caf884467a11d45bcc866c873eca655c60a
Brandon Soncarty <bsoncarty@ewu.edu>
2022-11-16 10:36:36 -0800

    Started methods in Creational

--- src/sbw/project/cli/parser/CommandParserCreational.java
+++ src/sbw/project/cli/parser/CommandParserCreational.java
@@ -12,5 +12,39 @@ public class CommandParserCreational extends CommandParser{
      */
     public CommandParserCreational(ActionSet actionSet, String text) {
         super(actionSet, text);
+        String[] textArr = text.split(" ");
+        if(textArr[1].equalsIgnoreCase("RUDDER")){
+            CreateRudder(actionSet, textArr);
+        }else if(textArr[1].equalsIgnoreCase("ELEVATOR")){
+            CreateElevator(actionSet, textArr);
+        }else if(textArr[1].equalsIgnoreCase("AILERON")) {
+            CreateAileron(actionSet, textArr);
+        }else if(textArr[1].equalsIgnoreCase("FLAP")) {
+            CreateFlap(actionSet, textArr);
+        }else if(textArr[1].equalsIgnoreCase("ENGINE")) {
+            CreateEngine(actionSet, textArr);
+        }else if(textArr[1].equalsIgnoreCase("GEAR")) {
+            CreateGear(actionSet, textArr);
+        }else{
+            System.out.println("Error in CommandParserStructural.");
+        }
+    }
+
+    private void CreateGear(ActionSet actionSet, String[] textArr) {
+    }
+
+    private void CreateEngine(ActionSet actionSet, String[] textArr) {
+    }
+
+    private void CreateFlap(ActionSet actionSet, String[] textArr) {
+    }
+
+    private void CreateAileron(ActionSet actionSet, String[] textArr) {
+    }
+
+    private void CreateElevator(ActionSet actionSet, String[] textArr) {
+    }
+
+    private void CreateRudder(ActionSet actionSet, String[] textArr) {
     }
 }
