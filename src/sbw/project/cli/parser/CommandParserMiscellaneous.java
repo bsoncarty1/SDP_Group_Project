8fee312dd8327d382ccca9fe7d17d38a944db476
j0shh3ss <joshuahess11@gmail.com>
2022-11-15 22:51:49 -0800

    Added methods

--- src/sbw/project/cli/parser/CommandParserMiscellaneous.java
+++ src/sbw/project/cli/parser/CommandParserMiscellaneous.java
@@ -12,5 +12,47 @@ public class CommandParserMiscellaneous extends CommandParser{
      */
     public CommandParserMiscellaneous(ActionSet actionSet, String text) {
         super(actionSet, text);
+        String[] textArr = text.split(" ");
+        switch(textArr){
+            case textArr[0].equalsIgnoreCase("@CLOCK") :
+                clock(actionSet, textArr);
+                break;
+            case textArr[0].equalsIgnoreCase("@EXIT") :
+                exit(actionSet);
+                break;
+            case textArr[0].equalsIgnoreCase("@WAIT") :
+                wait(actionSet, textArr);
+                break;
+        }
+    }
+
+    /*
+     * @Clock <rate>
+     * Sets  the system clock speed to rate ticks per second.
+     * This calls submitCommand() with an instance of CommandDoSetClockRate(Rate rate)
+     */
+    //@?
+    public void clock (ActionSet as, String[] textArr){
+        double rate = textArr[1]; //might need to adjust, also not entirely sure what Rate rate is asking for
+        as.submitCommand().CommandDoSetClockRate(rate);
+    }
+
+    /*
+     * @EXIT
+     * Exits the system
+     * This calls submitCommand() with an instance of CommandDoExit().
+     */
+    public void exit (ActionSet as) {
+        as.submitCommand().CommandDoExit();
+    }
+
+    /*
+     * @WAIT <rate>
+     * Waits rate milliseconds before executing the next behavioral command. This command is not valid until after @commit.
+     * This calls submitCommand() with an instance of CommandDoWait(Rate rate).
+     */
+    public void wait(ActionSet as, String[] textArr) {
+        double rate = textArr[1]; //Might need to adjust
+        as.submitCommand().CommandDoWait(rate);
     }
 }
