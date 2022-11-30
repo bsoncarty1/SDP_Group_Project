dca885a7b3a989e0eeae31ba84134a3659f24eb8
Brandon Soncarty <bsoncarty@ewu.edu>
2022-11-30 15:21:45 -0800

    Added more creational commands to Tester

--- src/sbw/project/cli/parser/Tester.java
+++ src/sbw/project/cli/parser/Tester.java
@@ -9,7 +9,9 @@ public class Tester {
         CommandLineInterface commandLineInterface = new CommandLineInterface();
 
         //Creational commands here:
-        commandLineInterface.processInput("CREATE RUDDER id1 WITH LIMIT 10 SPEED 10 ACCELERATION 10");
+        commandLineInterface.processInput("CREATE RUDDER rudder1 WITH LIMIT 45 SPEED 10 ACCELERATION 0.1");
+        commandLineInterface.processInput("CREATE ELEVATORS elevator1 AND elevator2 WITH LIMIT 30, SPEED 10, AND ACCELERATION 0.1");
+        commandLineInterface.processInput("CREATE ELEVATOR elevator3 WITH LIMIT 30, SPEED 10 ACCELERATION 0.15");
     }
 
 }
